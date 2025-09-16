151ec6f85b7f77e26fbd4af59aaddd55416d31e0
root <root@localhost>
2025-09-16 02:23:59 +0000

    Book Collection 4

---
+++ app/models/user_book.rb
@@ -0,0 +1,8 @@
+class UserBook < ApplicationRecord
+  belongs_to :user
+  belongs_to :book
+
+  validates :user_id, presence: true
+  validates :book_id, presence: true
+  validates :book_id, uniqueness: { scope: :user_id }
+end
